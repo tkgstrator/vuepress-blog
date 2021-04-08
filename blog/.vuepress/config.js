6bf9b16dca8a89372042d200e2f47344e9cc6a72
tkgstrator <nasawake.am@gmail.com>
2021-04-08 11:47:01 +0900

    - プロフィールなどの見た目を変更

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,61 +1,140 @@
 module.exports = {
+  // title of website
   title: "えいむーと愉快な仲間たち",
-  description: "えいむーの技術メモ",
-  theme: "@vuepress/theme-blog", // OR shortcut: @vuepress/blog
+
+  // description of website
+  description: "えいむーの備忘録です",
+
+  // language of website
+  locales: {
+    "/": {
+      lang: "ja",
+    },
+  },
+
+  // head information of website
+  head: [
+    // need favicon
+    ["meta", { name: "og:title", content: "えいむーと愉快な仲間たち" }],
+    ["meta", { name: "og:url", content: "https://tkgling.netlify.app/" }],
+    ["meta", { name: "og:type", content: "website" }],
+    [
+      "meta",
+      {
+        name: "og:description",
+        content: "えいむーの備忘録です",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "og:image",
+        content: "https://avatars.githubusercontent.com/u/29420801?v=4",
+      },
+    ],
+  ],
+
+  // markdown config
+  markdown: {
+    lineNumbers: true,
+    linkfy: true,
+  },
+
+  // theme to use
+  theme: "meteorlxy", // OR shortcut: @vuepress/blog
+
   themeConfig: {
-    /**
-     * Ref: https://vuepress-theme-blog.ulivz.com/#modifyblogpluginoptions
-     */
-    modifyBlogPluginOptions(blogPluginOptions) {
-      return blogPluginOptions;
-    },
-    head: [
-      [
-        "link",
-        {
-          rel: "stylesheet",
-          href:
-            "https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&display=swap",
+    // language of this theme
+    lang: "ja-JP",
+
+    // personal information
+    personalInfo: {
+      nickname: "tkgling",
+
+      description:
+        "神保町に本を詰まったビルを所有している紙使いのお姉さんがいます",
+
+      email: "nasawake.am@gmail.com",
+
+      location: "あそこ",
+
+      avatar: "https://avatars.githubusercontent.com/u/29420801?v=4",
+
+      sns: {
+        // github account and link
+        github: {
+          account: "tkgstrator",
+          link: "https://github.com/tkgstrator",
         },
-      ],
-    ],
-    /**
-     * Ref: https://vuepress-theme-blog.ulivz.com/#nav
-     */
-    nav: [
-      {
-        text: "記事一覧",
-        link: "/",
+
+        // twitter account and link
+        twitter: {
+          account: "tkgling",
+          link: "https://twitter.com/tkgling",
+        },
+
+        // instagram account and link
+        // instagram: {
+        //   account: '',
+        //   link: '',
+        // },
       },
-      {
-        text: "タグ",
-        link: "/tag/",
+    },
+
+    // header config
+    header: {
+      background: {
+        // use random pattern
+        useGeo: true,
       },
-    ],
-    /**
-     * Ref: https://vuepress-theme-blog.ulivz.com/#footer
-     */
+
+      // show title in the header or not
+      showTitle: true,
+    },
+
+    // footer config
     footer: {
-      contact: [
-        {
-          type: "github",
-          link: "https://github.com/ulivz",
-        },
-        {
-          type: "twitter",
-          link: "https://twitter.com/_ulivz",
-        },
-      ],
-      copyright: [
-        {
-          text: "Privacy Policy",
-          link: "https://policies.google.com/privacy?hl=en-US",
-        },
-        {
-          text: "MIT Licensed | Copyright © 2018-present Vue.js",
-          link: "",
-        },
-      ],
+      // show 'Powered by VuePress' or not
+      poweredBy: true,
+
+      // show the theme
+      poweredByTheme: true,
     },
+
+    // info card config
+    infoCard: {
+      // the background of the info card's header
+      headerBackground: {
+        // use random pattern
+        useGeo: true,
+      },
+    },
+
+    // show the last updated time of posts
+    lastUpdated: true,
+
+    // enable smooth scrolling or not
+    smoothScroll: true,
+
+    // pagination config
+    pagination: {
+      perPage: 10,
+    },
+
+    // comment config(vssue)
+    comments: {
+      platform: "github",
+      owner: "tkgling",
+      repo: "xxx",
+      clientId: "xxx",
+      clientSecret: "xxx",
+      autoCreateIssue: process.env.NODE_ENV !== "development",
+    },
+
+    // the content of navbar links
+    nav: [
+      { text: "Home", link: "/", exact: true },
+      { text: "Posts", link: "/posts/", exact: false },
+    ],
   },
 };
