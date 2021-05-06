a13613406fda4c19ab1d0e8d1cdf98b6ee2c76cc
tkgstrator <nasawake.am@gmail.com>
2021-05-06 13:03:28 +0900

    - 不要なコンポーネントの削除

--- blog/.vuepress/plugins/comments.js
+++
@@ -1,9 +0,0 @@
-const comments = require("../_data/comments.json")
-
-module.exports = (options, ctx) => {
-  return {
-    extendPageData($page) {
-      $page.comments = comments[$page.key] || []
-    }
-  }
-}
