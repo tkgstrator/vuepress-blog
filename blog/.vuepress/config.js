5b9ce3a0204419ca06ba166224f0887def21850e
devonly <nasawake.am@gmail.com>
2021-11-30 13:50:39 +0900

    - 数式表示に対応

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -4,4 +4,25 @@ module.exports = {
   themeConfig: {
     // ...
   },
+  head: [
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "https://cdn.jsdelivr.net/npm/katex@0.11.0/dist/katex.min.css",
+      },
+    ],
+  ],
+  markdown: {
+    extendMarkdown: (md) => {
+      md.use(require("@iktakahiro/markdown-it-katex"), {
+        throwOnError: false,
+        errorColor: "#cc0000",
+        macros: {
+          "\\Z": "\\mathbb{Z}",
+          "*": "\\times",
+        },
+      });
+    },
+  },
 };
